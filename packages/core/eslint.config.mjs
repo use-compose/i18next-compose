a42b4f3725a5e78cf41a0c6e9f36a8beb33323aa
arthur <plazanet.art@gmail.com>
2025-02-20 01:47:39 +0100

    ESLint Prettier config

---
+++ packages/core/eslint.config.mjs
@@ -0,0 +1,4 @@
+import eslintConfigBase from '../../eslint.config.mjs';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [...eslintConfigBase];
--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
+  plugins: [dts()],
+
   build: {
     lib: {
-      entry: './lib/main.ts',
-      name: 'Counter',
-      fileName: 'counter',
+      entry: './src/index.ts',
+      name: 'i18next-compose',
+      fileName: 'index',
     },
   },
-})
+});
