7807ad8c47f58ab8fc5b1977f69d681425f37f63
arthur <plazanet.art@gmail.com>
2025-03-03 17:54:57 +0100

    Fix lint

--- example/nuxt/app.vue
+++ example/nuxt/app.vue
@@ -6,6 +6,9 @@
 </template>
 
 <script setup>
+// TODO: Causes issues in lint - fix this with proper ESLint / Prettier config
+import { useI18n } from './composables';
+
 const i18n = useI18n();
 console.log('📟 - i18n → ', i18n);
 </script>
