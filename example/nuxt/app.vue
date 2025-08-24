4e6fd190c2b1860c21fa1386d91400f27f45679c
arthur <plazanet.art@gmail.com>
2025-08-24 20:03:51 +0200

    Fix lint

--- example/nuxt/app.vue
+++ example/nuxt/app.vue
@@ -14,10 +14,10 @@
 // TODO: Causes issues in lint - fix this with proper ESLint / Prettier config
 import { useI18nLocal } from './composables/use-i18n';
 import { getFlagEmoji } from 'i18next-compose';
-import type { ComposeContext } from 'i18next-compose';
-const { i18n, cT } = useI18nLocal('home');
+// import type { ComposeContext } from 'i18next-compose';
+const { cT } = useI18nLocal('home');
 // if (import.meta.client) {
-// eslint-disable-next-line no-console
+
 // console.log('📟 - SSR - skipping i18n setup');
 // const nuxtApp = useNuxtApp();
 // const i18nContext = nuxtApp.$i18nContext as ComposeContext;
@@ -27,7 +27,7 @@ const { i18n, cT } = useI18nLocal('home');
 // const cT = useI18n('home').cT;
 // console.log('📟 - useI18n → ', useI18n.useI18n('home'));
 // const { cT } = useI18n.useI18n('home');
-// eslint-disable-next-line no-console
+
 // console.log('📟 - i18n → ', useI18n);
 // }
 
