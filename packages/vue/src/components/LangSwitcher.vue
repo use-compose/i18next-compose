2237fb7c38b65833e30372b776c8b3c54615e2e3
arthur-plazanet <72502545+arthur-plazanet@users.noreply.github.com>
2025-09-15 08:32:04 +0200

    Init Vue package (#72)
    * Few fixes vite config paths
    
    * Update docs API 15-09-2025
    
    * Init Vue package
    
    * Publish first Vue package
    
    * Fix engine

---
+++ packages/vue/src/components/LangSwitcher.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <span>DE: {{ getFlagEmoji('de') }}</span>
+    <span>EN: {{ getFlagEmoji('gb') }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { getFlagEmoji } from 'i18next-compose';
+</script>
+
+<style scoped></style>
---
+++ packages/vue/vite.config.ts
@@ -0,0 +1,48 @@
+import { fileURLToPath, URL } from 'node:url';
+import dts from 'vite-plugin-dts';
+
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
+import vueDevTools from 'vite-plugin-vue-devtools';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+    dts({
+      entryRoot: './src',
+      tsconfigPath: fileURLToPath(new URL('./tsconfig.json', import.meta.url)),
+      include: ['./src/**/*'],
+      outDir: './dist',
+      rollupTypes: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    lib: {
+      entry: fileURLToPath(new URL('./src/index.ts', import.meta.url)),
+      name: 'i18next-compose-vue',
+      fileName: (format) => {
+        if (format === 'es') return 'index.mjs';
+        if (format === 'cjs') return 'index.cjs';
+        return `index.${format}.js`;
+      },
+      formats: ['es', 'cjs'],
+    },
+    outDir: 'dist',
+    emptyOutDir: true,
+    // TODO: https://github.com/vitejs/vite/discussions/2978#discussioncomment-5276995 ?
+    // generate .vite/manifest.json in outDir
+    // manifest: true,
+    rollupOptions: {
+      external: ['@use-compose/i18next-core', 'vue'],
+      output: { globals: { vue: 'Vue' } },
+    },
+    target: 'esnext',
+  },
+});
